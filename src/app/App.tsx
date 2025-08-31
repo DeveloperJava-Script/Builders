2938ba774bf28335aaa4bae878f052420d6a7dda
DeveloperJava-Script <andrey172086@gmail.com>
2025-08-31 20:22:40 +0300

    fixes11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,22 @@ function App() {
   const telegram = useTelegram();
 
   useEffect(() => {
+    telegram.ready();
     telegram.expand();
+
+    // Определяем платформу устройства
+    const platform = telegram.platform;
+
+    if (platform === 'android' || platform === 'ios') {
+      telegram.requestFullscreen();
+    }
+
     telegram.disableVerticalSwipes();
+    // telegram.setBackgroundColor("#000");
+    // telegram.setHeaderColor("#000");
+    telegram.enableClosingConfirmation();
+    telegram.BackButton.hide();
+    telegram.MainButton.hide();
   }, []);
 
   return <Layout />;
